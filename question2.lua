80bf5427b22cbb07cafee1117a395c0a4764cbb9
exlted <benk@exlted.com>
2024-04-11 20:33:14 -0500

    Question 2 First Pass

--- question2.lua
+++ question2.lua
@@ -1,7 +1,17 @@
 function printSmallGuildNames(memberCount)
   -- this method is supposed to print names of all guilds that have less than memberCount max members
   local selectGuildQuery = "SELECT name FROM guilds WHERE max_members < %d;"
+  -- Makes a query that stores into result (accessible referencing resultId)
   local resultId = db.storeQuery(string.format(selectGuildQuery, memberCount))
-  local guildName = result.getString("name")
-  print(guildName)
+  -- Before: This is wrong, for one, it doesn't reference resultId
+  --  Additionally, it doesn't free the memory from the query
+  --  Third of all, it doesn't loop over anything, so this will only print 1 name
+
+  repeat
+  {
+    local guildName = result.getDataString(resultId, "name")
+    print(guildName)
+  } until (!result.next(resultId))
+
+  result.free(resultId)
 end
