a15a7906c0ab12c0ef1bbe15e9a6712c6893cf8e
exlted <benk@exlted.com>
2024-04-11 21:32:11 -0500

    Question 4 First Pass

--- question4.cpp
+++ question4.cpp
@@ -1,12 +1,7 @@
-void Game::addItemToPlayer(const std::string& recipient, uint16_t itemId)
-{
-  Player* player = g_game.getPlayerByName(recipient);
-  if (!player) {
-    player = new Player(nullptr);
-    if (!IOLoginData::loadPlayerByName(player, recipient)) {
-      return;
-    }
-  }
+// By refactoring a function out that takes in a Player*, we're able to avoid duplicating this code block
+//  while still utilizing it no matter which code path we end up using
+void Game::innerAddItemToPlayer(Player* player, uint16_t itemId)
+{  
   Item* item = Item::CreateItem(itemId);
   if (!item) {
      return;
@@ -14,5 +9,25 @@ void Game::addItemToPlayer(const std::string& recipient, uint16_t itemId)
   g_game.internalAddItem(player->getInbox(), item, INDEX_WHEREEVER, FLAG_NOLIMIT);
   if (player->isOffline()) {
     IOLoginData::savePlayer(player);
+  }
+}
+
+void Game::addItemToPlayer(const std::string& recipient, uint16_t itemId)
+{
+  Player* player = g_game.getPlayerByName(recipient);
+  if (player) {
+    innerAddItemToPlayer(player, itemId);
+  }
+  else 
+  {
+    // Calling "new" here without a "delete" is what's causing the memory leak
+    //player = new Player(nullptr);
+    // So, instead of calling new, just make the memory on the stack instead of the heap
+    Player tmpPlayer = Player(nullptr);
+    if (!IOLoginData::loadPlayerByName(&tmpPlayer, recipient)) {
+      return;
+    }
+
+    innerAddItemToPlayer(&tmpPlayer, itemId);
   }
 }
